10786b0ddc833dbf39479eb2ae80c7f9667fef62
SiuhiuY <279433678+SiuhiuY@users.noreply.github.com>
2025-11-09 16:30:51 +0100

    refactor: move data loading into a function to be compatible with binary classification

--- eegnet_baseline.py
+++ eegnet_baseline.py
@@ -11,90 +11,137 @@ from sklearn.metrics import (confusion_matrix, accuracy_score,
 from src.models.EEGNet import EEGNet
 from src.eeg_processor import EEGProcessor
 from src.subject_processor import SubjectProcessor
-import src.util as util
+# import src.util as util
 
 
 # Set random seed for reproducibility
-util.reproducibility(123)
+# util.reproducibility(123)
 
-# Define directories and labels
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(ROOT_DIR, "data")
-PROCESSED_DIR = os.path.join(DATA_DIR, "derivatives")
-LABEL_PATH = os.path.join(DATA_DIR, "participants.tsv")
-LABEL_MAP = {"A": 0, "F": 1, "C": 2}
 
-# Initialise SubjectProcessor to get directories of all subjects
-process_subjects = SubjectProcessor(DATA_DIR)
-process_subjects.find_all_subjects()
+def load_eeg_data(label_map):
+    """Load EEG data for all subjects and prepare for EEGNet.
 
-# Load subject IDs and labels
-label_df = pd.read_csv(LABEL_PATH, sep="\t")
-label_dict = label_df.set_index("participant_id")['Group'].to_dict()
+    Args:
+        label_map (dict): Mapping of class names to integer labels.
 
-# Map class names to integer labels
-label_to_int = {
-        subj_id: LABEL_MAP[group]
-        for subj_id, group in label_dict.items()
-    }
+    Returns:
+        tuple: (X_all, y_all, subjects_all)
+            where X_all is a numpy array of shape
+            (n_total_epochs, n_channels, n_times, 1),
+            y_all is a numpy array of shape (n_total_epochs, n_classes),
+            and subjects_all is a numpy array of shape (n_total_epochs,).
+    """
 
-# Load EEG data
-all_features = []
-all_labels = []
-all_subjects = []
+    # Define directories and labels
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR = os.path.join(ROOT_DIR, "data")
+    PROCESSED_DIR = os.path.join(DATA_DIR, "derivatives")
+    LABEL_PATH = os.path.join(DATA_DIR, "participants.tsv")
 
-# Loop through each subject and process their EEG data
-for subj_dir in process_subjects.subject_dirs:
+    if not os.path.exists(PROCESSED_DIR):
+        raise FileNotFoundError(
+            f"Processed data directory {PROCESSED_DIR} not found."
+        )
 
-    subj_id = os.path.basename(subj_dir)
-    print(f"Processing subject: {subj_id}")
-    subj_path = os.path.join(subj_dir, "eeg",
-                             f"{subj_id}_task-eyesclosed_eeg.set")
+    # Initialise SubjectProcessor to get directories of all subjects
+    process_subjects = SubjectProcessor(DATA_DIR)
+    process_subjects.find_all_subjects()
 
-    processor = EEGProcessor(PROCESSED_DIR, subj_path)
-    processor.load_data()
+    # Load subject IDs and labels
+    label_df = pd.read_csv(LABEL_PATH, sep="\t")
+    label_dict = label_df.set_index("participant_id")['Group'].to_dict()
 
-    # Epoch the data into 4-second segments with 2-second overlap
-    processor.epoch_data(duration=4.0, overlap=2.0)
-    signals = processor.epochs
+    # Get valid groups from label_map
+    valid_groups = set(label_map.keys())
 
-    # Extract epoched data as NumPy array
-    # Shape: (n_epochs, n_channels, n_times per epoch)
-    subj_feature = signals.get_data()
+    # Map class names to integer labels
+    label_to_int = {
+            subj_id: label_map[group]
+            for subj_id, group in label_dict.items() if group in valid_groups
+        }
 
-    # Get the number of epochs and class label for the subject
-    n_epochs = subj_feature.shape[0]
-    label = label_to_int[subj_id]
+    # Load EEG data
+    all_features = []
+    all_labels = []
+    all_subjects = []
 
-    # Replicate labels and subject IDs for each epoch
-    subj_labels = [label] * n_epochs
-    subj_ids = [subj_id] * n_epochs
+    # Loop through each subject and process their EEG data
+    for subj_dir in process_subjects.subject_dirs:
+        subj_id = os.path.basename(subj_dir)
 
-    # Append features, labels, and subject IDs
-    all_features.extend(subj_feature)
-    all_labels.extend(subj_labels)
-    all_subjects.extend(subj_ids)
+        if subj_id in label_to_int:
+            subj_path = os.path.join(subj_dir, "eeg",
+                                     f"{subj_id}_task-eyesclosed_eeg.set")
+            if not os.path.exists(subj_path):
+                raise FileNotFoundError(
+                    f"EEG file for subject {subj_id} not found at {subj_path}."
+                )
+            print(f"Loading data for subject {subj_id}...")
 
-# Convert lists to numpy arrays
-X_all = np.array(all_features, dtype=np.float64)
-y_all = np.array(all_labels, dtype=np.int64)
-subjects_all = np.array(all_subjects, dtype=object)
+            processor = EEGProcessor(PROCESSED_DIR, subj_path)
+            processor.load_data()
+
+            # Epoch the data into 4-second segments with 2-second overlap
+            processor.epoch_data(duration=4.0, overlap=2.0)
+            signals = processor.epochs
+
+            # Extract epoched data as NumPy array
+            # Shape: (n_epochs, n_channels, n_times per epoch)
+            subj_feature = signals.get_data()
+
+            # Get the number of epochs and class label for the subject
+            n_epochs = subj_feature.shape[0]
+            label = label_to_int[subj_id]
+
+            # Replicate labels and subject IDs for each epoch
+            subj_labels = [label] * n_epochs
+            subj_ids = [subj_id] * n_epochs
+
+            # Append features, labels, and subject IDs
+            all_features.append(subj_feature)
+            all_labels.extend(subj_labels)
+            all_subjects.extend(subj_ids)
+
+    # Convert lists to numpy arrays
+    X_all = np.concatenate(all_features, axis=0).astype(np.float32)
+    y_all = np.array(all_labels, dtype=np.int64)
+    subjects_all = np.array(all_subjects, dtype=object)
+
+    return X_all, y_all, subjects_all
+
+
+# Define label path and label map
+AD_FTD_CN = {"A": 0, "F": 1, "C": 2}
+AD_CN = {"A": 0, "C": 1}
+FTD_CN = {"F": 0, "C": 1}
+
+X_all, y_all, subjects_all = load_eeg_data(
+    FTD_CN
+)
+
+print(X_all.shape, y_all.shape, subjects_all.shape)
+print(np.unique(y_all))
+print(np.unique(subjects_all))
 
 # Reshape features for EEGNet input
 # EEGNet expects input shape: (n_epochs, n_channels, n_times, 1)
-kernels, chans, samples = 1, 19, 151
-X_all = X_all.reshape(X_all.shape[0], chans, samples, kernels)
+# kernels, chans, samples = 1, 19, 151
+# X_all = X_all.reshape(X_all.shape[0], chans, samples, kernels)
 
-# Convert labels to one-hot encoding
-num_classes = len(LABEL_MAP)
-y_all = to_categorical(y_all, num_classes=num_classes)
+# # Convert labels to one-hot encoding
+# if binary:
+#     num_classes = 2
+# else:
+#     num_classes = 3
 
-print(f"Total samples: {X_all.shape[0]}")
-print(f"Feature shape: {X_all.shape[1:]}")  # (n_channels, n_times)
-print(f"Labels shape: {y_all.shape}")
-print(f"Subjects shape: {subjects_all.shape}")
-print(X_all.shape, y_all.shape, subjects_all.shape)
-print("Unique labels:", np.unique(np.argmax(y_all, axis=1)))
-print("Unique subjects:", np.unique(subjects_all))
-print("Sample feature shape:", X_all[0].shape)
-print("Sample label (one-hot):", y_all[0])
+# y_all = to_categorical(y_all, num_classes=num_classes)
+
+# print(f"Total samples: {X_all.shape[0]}")
+# print(f"Feature shape: {X_all.shape[1:]}")  # (n_channels, n_times)
+# print(f"Labels shape: {y_all.shape}")
+# print(f"Subjects shape: {subjects_all.shape}")
+# print(X_all.shape, y_all.shape, subjects_all.shape)
+# print("Unique labels:", np.unique(np.argmax(y_all, axis=1)))
+# print("Unique subjects:", np.unique(subjects_all))
+# print("Sample feature shape:", X_all[0].shape)
+# print("Sample label (one-hot):", y_all[0])
