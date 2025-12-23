ee3e5e1b93b5ddc3a2e56929e39ab19cc760a415
SiuhiuY <279433678+SiuhiuY@users.noreply.github.com>
2025-12-23 14:35:58 +0100

    fix: remove unused fit and calculate_metrics methods from ModelTrainer

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -13,36 +13,30 @@ from torchmetrics import (
 class ModelTrainer:
     """Class to handle model training, validation, and testing."""
     def __init__(
-        self, model, train_loader, test_loader,
-        optimizer, criterion, device, threshold=0.5
+        self, model, train_loader, eval_loader,
+        criterion, device, optimizer=None, threshold=0.5
     ):
         """Initialise the ModelTrainer.
 
         Args:
             model (torch.nn.Module): The neural network model to train.
             train_loader (DataLoader): DataLoader for training data.
-            val_loader (DataLoader): DataLoader for validation data.
-            test_loader (DataLoader): DataLoader for test data.
-            optimizer (torch.optim.Optimizer): Optimizer for training.
+            eval_loader (DataLoader): DataLoader for validation / test data.
             criterion (torch.nn.Module): Loss function.
             device (torch.device): Device to run the model on.
+            optimizer (torch.optim.Optimizer): Optimizer for training.
             threshold (float): Threshold for converting probabilities to
                 class labels.
         """
         self.model = model
         self.train_loader = train_loader
-        self.test_loader = test_loader
+        self.eval_loader = eval_loader
         self.device = device
         self.threshold = threshold
         self.criterion = criterion
         self.optimizer = optimizer
-
-        self.accuracy = Accuracy(task="binary").to(device)
-        self.precision = Precision(task="binary").to(device)
-        self.recall = Recall(task="binary").to(device)
-        self.f1_score = F1Score(task="binary").to(device)
-        self.auroc = AUROC(task="binary").to(device)
-        self.confusion_matrix = ConfusionMatrix(task="binary").to(device)
+        self.train_accuracy = Accuracy(task="binary").to(device)
+        self.eval_accuracy = Accuracy(task="binary").to(device)
 
     def train_one_epoch(self):
         """Train the model for one epoch.
@@ -51,12 +45,15 @@ class ModelTrainer:
             float: Average training loss for the epoch.
             float: Training accuracy for the epoch.
         """
+        if self.optimizer is None:
+            raise ValueError("Optimizer must be provided for training.")
+
         # Set model to training mode
         self.model.train()
 
         # Initialise running loss
         running_loss = 0.0
-        acc = self.accuracy
+        acc = self.train_accuracy
 
         # Training in batches
         for features, labels, _ in self.train_loader:
@@ -89,11 +86,11 @@ class ModelTrainer:
             acc.update(preds, labels.view(-1, 1))
 
         # Compute average loss and accuracy for the epoch
-        epoch_loss = running_loss / len(self.train_loader)
-        accuracy = acc.compute()
+        train_loss = running_loss / len(self.train_loader)
+        train_acc = acc.compute()
         acc.reset()
 
-        return epoch_loss, accuracy
+        return train_loss, train_acc
 
     def evaluate_one_epoch(self):
         """Evaluate the model on the validation set for one epoch.
@@ -107,11 +104,11 @@ class ModelTrainer:
 
         # Initialise test metric
         running_loss = 0.0
-        acc = self.accuracy
+        acc = self.eval_accuracy
 
         # Disable gradient backpropagation for evaluation
         with torch.no_grad():
-            for features, labels, _ in self.test_loader:
+            for features, labels, _ in self.eval_loader:
 
                 features = features.to(self.device)
                 labels = labels.to(self.device)
@@ -124,36 +121,11 @@ class ModelTrainer:
                 preds = (pred_probs >= self.threshold).float()
                 acc.update(preds, labels.view(-1, 1))
 
-        epoch_loss = running_loss / len(self.test_loader)
-        accuracy = acc.compute()
+        eval_loss = running_loss / len(self.eval_loader)
+        eval_acc = acc.compute()
         acc.reset()
 
-        return epoch_loss, accuracy
-
-    def fit(self, num_epochs):
-        """Train and validate the model for a given number of epochs.
-
-        Args:
-            num_epochs (int): Number of epochs to train the model.
-
-        Returns:
-            dict: Training history containing loss and accuracy for
-                training and validation sets.
-        """
-        history = {
-            "train_loss": [],
-            "train_acc": [],
-            "val_loss": [],
-            "val_acc": [],
-        }
-        for epoch in range(num_epochs):
-            train_loss, train_acc = self.train_one_epoch()
-            val_loss, val_acc = self.evaluate_one_epoch()
-            history["train_loss"].append(train_loss)
-            history["train_acc"].append(train_acc.item())  # convert tensor to float
-            history["val_loss"].append(val_loss)
-            history["val_acc"].append(val_acc.item())
-        return history
+        return eval_loss, eval_acc
 
     def predict(self):
         """Generate predictions on the test set.
@@ -167,66 +139,21 @@ class ModelTrainer:
         y_preds = []
         y_labels = []
         with torch.no_grad():
-            for features, labels, _ in self.test_loader:
+            for features, labels, _ in self.eval_loader:
                 features = features.to(self.device)
                 labels = labels.to(self.device)
 
                 outputs = self.model(features)
                 pred_probs = torch.sigmoid(outputs)
+                preds = (pred_probs >= self.threshold).float()
+
                 # Move to CPU to save the results
                 y_pred_probs.extend(pred_probs.cpu().numpy())
-                preds = (pred_probs >= self.threshold).float()
                 y_preds.extend(preds.cpu().numpy())
                 y_labels.extend(labels.cpu().numpy())
+
         return {
             "y_pred_probs": y_pred_probs,
             "y_pred": y_preds,
             "y_true": y_labels
-        }
-
-    def calculate_metrics(self):
-        """Calculate evaluation metrics on the test set.
-
-        Returns:
-            dict: Dictionary containing accuracy, precision, recall,
-                F1-score, confusion matrix, and AUC.
-        """
-        self.model.eval()
-        acc = self.accuracy
-        precision = self.precision
-        recall = self.recall
-        f1 = self.f1_score
-
-        with torch.no_grad():
-            for features, labels, _ in self.test_loader:
-                features = features.to(self.device)
-                labels = labels.to(self.device)
-
-                outputs = self.model(features)
-                pred_probs = torch.sigmoid(outputs)
-                preds = (pred_probs >= self.threshold).float()
-
-                # Update metrics by batch
-                acc.update(preds, labels.view(-1, 1))
-                precision.update(preds, labels.view(-1, 1))
-                recall.update(preds, labels.view(-1, 1))
-                f1.update(preds, labels.view(-1, 1))
-
-        # Compute metrics for entire test set
-        test_acc = acc.compute()
-        test_precision = precision.compute()
-        test_recall = recall.compute()
-        test_f1 = f1.compute()
-
-        # Reset metrics
-        acc.reset()
-        precision.reset()
-        recall.reset()
-        f1.reset()
-
-        return {
-            "accuracy": test_acc,
-            "precision": test_precision,
-            "recall": test_recall,
-            "f1_score": test_f1
         }
